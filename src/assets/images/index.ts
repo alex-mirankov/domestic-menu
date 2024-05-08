6529f8599a222b1a2c258c63b08360d293b873ff
Aliaksandr Mirankou <aliaksandr_mirankou@epam.com>
2024-05-08 16:15:41 +0400

    add soup and breakfast menu

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,10 +1,50 @@
 import greekSalad from './greek-salad.jpeg';
 import tomatoSoup from './tomato-soup.jpeg';
 import oladies from './oladies.jpg';
+import avocadoSandwich from './avocado_sandwich.jpeg';
+import blini from './blini.jpeg';
+import borsch from './borsch.webp';
+import chickenSoup from './chicken_soup.webp';
+import hotButer from './Goryachij_buterbrod_v_vafelnitse_na_kak-vkusno.jpg';
+import greekChia from './greek_chia.jpeg';
+import harcho from './harcho.webp';
+import kolbasaButer from './kolbasa_buter.jpeg';
+import manka from './manka.png';
+import meatballSoup from './meatballs_soup.jpeg';
+import omlette from './omlette.jpeg';
+import ovsyanayaKasha from './ovsyanaya-kasha-s-bananom.jpeg';
+import shakshuka from './shakshuka.jpeg';
+import sirnik from './sirnik.jpeg';
+import soupSausage from './soup_sausage.jpeg';
+import tvorog from './tvorog_s_bananom.jpeg';
+import waffels from './waffels.webp';
+import yaichnicaKolbasoi from './yaichnica_s_kolbasoi.jpeg';
+import zapekanka from './zapekanka.jpeg';
+import fishSoup from './fish_soup.jpeg';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
     greekSalad,
     tomatoSoup,
     oladies,
+    avocadoSandwich,
+    blini,
+    borsch,
+    chickenSoup,
+    hotButer,
+    greekChia,
+    harcho,
+    manka,
+    kolbasaButer,
+    meatballSoup,
+    omlette,
+    ovsyanayaKasha,
+    shakshuka,
+    sirnik,
+    soupSausage,
+    tvorog,
+    waffels,
+    yaichnicaKolbasoi,
+    zapekanka,
+    fishSoup
 };
